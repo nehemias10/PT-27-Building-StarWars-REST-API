1383bc231242810b919d827b0f8abda0689e591e
nehemias10 <nehemiasrivero1@gmail.com>
2022-08-19 01:21:53 +0000

    3

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
 app = Flask(__name__)
@@ -41,17 +41,31 @@ def handle_hello():
 
 @app.route('/people', methods=['GET'])
 def getPeople():
+    all_people = People.query.all()
+    serializados = list( map( lambda people: people.serialize(), all_people))
+    print(all_people)
+
     return jsonify({
         "mensaje": "Todos los Personajes",
-        "people": []
-    })
+        "people": serializados
+    }), 200
 
-@app.route('/people/<int:id>', methods=['GET'])
-def dinamycPeople(id):
-    return jsonify({
-        "id": id,
-        "mensaje": "Personajes dinamico"
-    })
+@app.route('/people/<int:idpeople>', methods=['GET'])
+def dinamycPeople(idpeople):
+    one = People.query.filter_by(uid=idpeople).first()
+    if(one):
+        return jsonify({
+            "id": idpeople,
+            "people": one.serialize()
+        }), 200
+        
+    else:
+        return jsonify({
+                "id": idpeople,
+                "people": "not found!"
+        }), 404
+
+
 
 @app.route('/planets', methods=['GET'])
 def getPlanets():
@@ -60,7 +74,7 @@ def getPlanets():
         "people": []
     })
 
-@app.route('/planets/<int:id>', methods=['GET'])
+@app.route('/planets/<int:idplanets>', methods=['GET'])
 def dinamycPlanets(id):
     return jsonify({
         "id": id,
@@ -79,6 +93,17 @@ def getUsersFav():
     return jsonify({
         "mensaje": "Get all the favorites that belong to the current user"
     })
+
+@app.route("/favorite/people/<int:people_id>", methods=['POST'])
+def postPeopleFav(people_id):
+    body = request.get_json() #recibir datos del usuario
+    #people_id = 4
+    #email = freddyloboq@gmail.com
+    newFav = FavPeople(user=body['email'], people= people_id)
+    db.session.add(newFav)
+    db.session.commit()
+    return "nuevo favorito agregado"
+    
 
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
 def postFavPlanet(planet_id):
